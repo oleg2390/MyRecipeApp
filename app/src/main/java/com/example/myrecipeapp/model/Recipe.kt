7ec6333142d08520bc4c877f6f617c465a5288c7
oleg <ip.pan@bk.ru>
2025-08-20 13:37:50 +0400

    RA-40 add cash for list recipes

--- app/src/main/java/com/example/myrecipeapp/model/Recipe.kt
+++ app/src/main/java/com/example/myrecipeapp/model/Recipe.kt
@@ -1,12 +1,16 @@
 package com.example.myrecipeapp.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
+@Entity(tableName = "recipes")
 @Parcelize
 @Serializable
 data class Recipe(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title: String,
     val ingredients: List<Ingredient>,
